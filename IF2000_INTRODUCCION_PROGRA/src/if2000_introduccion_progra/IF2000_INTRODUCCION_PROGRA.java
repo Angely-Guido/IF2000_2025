014381edecb3f9b189ec91472512397bb40476e1
unknown <guidoangely1987@gmail.com>
2025-08-19 11:48:31 -0600

    Martes 19 agosto

---
+++ IF2000_INTRODUCCION_PROGRA/src/if2000_introduccion_progra/IF2000_INTRODUCCION_PROGRA.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package if2000_introduccion_progra;
+
+/**
+ *
+ * @author estudiante
+ */
+public class IF2000_INTRODUCCION_PROGRA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        String name = "Liones";
+        int age = 40;
+        
+        System.out.println("Hello Word");
+        System.out.println("Name" + name);
+        System.out.println("Age" + age);
+    }
+    
+}
